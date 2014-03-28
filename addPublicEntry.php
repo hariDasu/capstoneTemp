1f681d7dff29de4dacbb3c91ee4ab7218b10d6c4
Srihari Rao <harirao3@gmail.com>
2014-03-27 20:34:53 -0400

    editing addEntries

--- addPublicEntry.php
+++ addPublicEntry.php
@@ -171,7 +171,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       
 
       <footer>
+          <nav class="navbar navbar-inverse" role="navigation">
+
+
         <p><a href="contact.php">&nbsp;Contact Us</a></p>
+              </nav>
       </footer>
     </div> <!-- /container -->
 
