42d45133cb2d291de080dc44820311419165db7d
Srihari Rao <harirao3@gmail.com>
2014-03-30 19:03:10 -0400

    editing editor

--- DataTablesEditor/sql/table.PROPERTY.sql
+++ DataTablesEditor/sql/table.PROPERTY.sql
@@ -5,18 +5,18 @@
 -- 
 
 CREATE TABLE `PROPERTY` (
-	`id` int(10) NOT NULL auto_increment,
+	`PROPID` int(10) NOT NULL auto_increment,
 	`BLOCK` varchar(255) default NULL,
 	`LOT` varchar(255) default NULL,
 	`WARD` varchar(255) default NULL,
+	`ADDRNUM` varchar(255) default NULL,
 	`STREET` varchar(255) default NULL,
 	`CITY` varchar(255) default NULL,
 	`ZIP` varchar(255) default NULL,
 	`STATE` varchar(255) default NULL,
 	`BOARDED` varchar(255) default NULL,
-	`SPOST` varchar(255) default NULL,
-	`LCOMMENT` varchar(255) default NULL,
 	`PDESC` varchar(255) default NULL,
-	PRIMARY KEY  (`id`)
+	`LCOMMENT` varchar(255) default NULL,
+	PRIMARY KEY  (`PROPID`)
 );
 
